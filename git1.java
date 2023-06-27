a9fb5e0013be125cdba2d01e419c1572744d64cc
rinku saini <80007809d@gmail.com>
2023-06-27 23:32:22 +0530

    assingnment 1

---
+++ git1.java
@@ -0,0 +1,34 @@
+public class git1 {
+    //  1. what is git ?
+    //ans. Git is a DevOps tool used for source code management. It is a free and open-source version control system used to handle small to very large projects efficiently. Git is used to tracking changes in the source code, enabling multiple developers to work together on non-linear development.
+
+    // 2. what don you understand by the term 'version control system'?
+    // ans Version control, also known as source control, is the practice of tracking and managing changes to software code. Version control systems are software tools that help software teams manage changes to source code over time.
+
+    // 3. What is github?
+    // ans. GitHub is a code hosting platform for version control and collaboration. 
+    //      It lets you and others work together on projects from anywhere. 
+
+    // 4. mention some popular git hosting services?
+    // ans.  applications, IT infrastructure components or functions that organizations access from external service 
+    //       providers, typically through an internet
+
+    // 5. different types of versioncontrol systems?
+    // ans there are two types of version control system 
+    //      1. centralised version control system 
+    //      2. distributed version control system
+     
+    //6. what benefits come with using git?
+    //ans One of the biggest advantages of Git is its branching capabilities. Unlike centralized version control systems, Git branches are cheap and easy to merge. 
+
+    //7. what is git repository?
+    //ans Repositories in GIT contain a collection of files of various different versions of a Project. These files are imported from the repository into the local server of the user for further updations and modifications in the content of the file.
+
+    //8. how can you initialize a repository in git?
+    //ans. 1. Open Terminal 
+    // 2. Navigate to the root directory of your project.
+    // 3. Initialize the local directory as a Git repository. By default, the initial branch is called main . ...
+    // 4. Add the files in your new local repository. ...
+    // 5. Commit the files that you've staged in your local repository.
+
+}
